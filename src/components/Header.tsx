66e42e6cd2277716bce209e274ee940a920d82bf
Debraj Roy <debrajroyofficial000@gmail.com>
2024-07-14 09:38:54 +0530

    Add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,41 @@
+import { NavLink } from "react-router-dom";
+import { AiFillShopping } from "react-icons/ai";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-skin-700 text-skin-100 py-4 shadow-md">
+      <div className="container mx-auto px-4 flex justify-between items-center">
+        <div className="text-2xl font-bold">
+          <NavLink to="/">Essentials</NavLink>
+        </div>
+        <nav className="flex gap-8 items-center">
+          <NavLink
+            to="/products"
+            className={({ isActive }) =>
+              isActive
+                ? "text-skin-100 font-semibold border-b-2 border-skin-300"
+                : "hover:text-skin-300 text-lg"
+            }
+          >
+            Products
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) =>
+              isActive
+                ? "text-skin-100 font-semibold border-b-2 border-skin-300 relative"
+                : "hover:text-skin-300 text-lg relative"
+            }
+          >
+            <AiFillShopping />
+            <div className="absolute bottom-3 left-3 w-7 h-7 text-center bg-skin-100 text-skin-700 text-md rounded-full">
+              0
+            </div>
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const RootLayout = () => {
   return (
     <div className="bg-skin-100 min-h-screen flex flex-col">
+      <Header />
       <main className="flex-1">
         <Outlet />
       </main>
